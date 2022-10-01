51002c79f2763eb11de38bcea4072be5d040995f
JAEYEONsss <y718j@ajou.ac.kr>
2022-10-01 20:20:45 +0900

    0 ~ 9까지의 합에서 vector 내에 있는 원소를 빼준다.

---
+++ 없는_숫자_더하기/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 45;
+
+    for(int i=0; i<numbers.size(); i++)
+        answer -= numbers[i];
+
+    return answer;
+}
+
+int main() {
+    vector<int> numbers = {1, 2, 3, 4, 6, 7, 8, 0};
+    cout << solution(numbers);
+    return 0;
+}
